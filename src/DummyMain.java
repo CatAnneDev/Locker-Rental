f59e9fd729ab7378533f914dedf6f01e2dce1757
Catherine <62034499+CatAnneDev@users.noreply.github.com>
2023-03-24 17:47:01 -0500

    Update DummyMain.java

--- src/DummyMain.java
+++ src/DummyMain.java
@@ -3,5 +3,10 @@ public class DummyMain
 	public static void main(String[] args)
 	{
 		// classes: Renter, Locker, Rental, Pin, User Interface
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input here");
+		String input = in.nextLine();
+
+		System.out.println(input);
 	}
 }

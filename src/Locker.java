a975423853fb7aad88615a4bf569c13a141f8763
bgarth <45801518+bgarth@users.noreply.github.com>
2023-03-24 17:16:25 -0500

    Update Locker.java
    revised attributes, added constructer, and added rental_status setter class

--- src/Locker.java
+++ src/Locker.java
@@ -3,13 +3,24 @@ class Locker
     public int locker_number;
     public boolean rental_status;
 
-    public void set_locker_number (int l_number)
+    public Rental (int lock_num, boolean rent_stat)
     {
-        locker_number = l_number;
+        locker_number = lock_num;
+        rental_status = rent_stat;
     }
-    public int get_locker_number ()
+
+    public void set_locker_number(int l_number)
+    {
+        locker_number = lock_num;
+    }
+    public int get_locker_number()
     {
         return locker_number;
+    }
+
+    public void set_rental_status(boolean rent_stat)
+    {
+        rental_status = rent_stat;
     }
 
     public boolean get_rental_status() {

82ad2e35e6b38607c604ab147868fd8b5b09e68c
bgarth <45801518+bgarth@users.noreply.github.com>
2023-03-24 17:21:13 -0500

    Update Locker.java
    Updated the locker class to include a Pin object. Only basic statements made for now

--- src/Locker.java
+++ src/Locker.java
@@ -2,11 +2,13 @@ class Locker
 {
     public int locker_number;
     public boolean rental_status;
+    Pin locker_pin;
 
-    public Rental (int lock_num, boolean rent_stat)
+    public Rental (int lock_num, boolean rent_stat, Pin pin)
     {
         locker_number = lock_num;
         rental_status = rent_stat;
+        locker_pin = pin;
     }
 
     public void set_locker_number(int l_number)
@@ -27,4 +29,12 @@ class Locker
         return rental_status;
     }
 
+    public void set_locker_pin(Pin pin)
+    {
+        locker_pin = pin;
+    }
+
+    public Pin getLocker_pin() {
+        return locker_pin;
+    }
 }

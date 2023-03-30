e0a7650f4c025d514a72f3e0be5750bbed89177b
taw5578 <taw5578@truman.edu>
2023-03-29 19:12:54 -0500

    Added user verification method
    User verification method added to be used when user requests to view a pin. This will ensure that only a valid user with the appropriate password will be able to access the pins

--- src/Pin.java
+++ src/Pin.java
@@ -2,20 +2,24 @@
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.swing.JOptionPane;
+
 public class Pin {
 	
-	Map<Integer, Integer[]> pins;
+	Map<Integer, Integer[]> temp_pins;
+
+	String password = "pass";
 	
 	
 	public Pin() {
 		// Generate temporary dummy pins
-		pins = new HashMap<>();
+		temp_pins = new HashMap<>();
 
 		// Store dummy pins in "lockers" 27, 28, and 29
 		Integer[] value = new Integer[] { 1111, 2222, 3333, 4444 };
-		pins.put(27, value);
-		pins.put(28, value);
-		pins.put(29, value);
+		temp_pins.put(27, value);
+		temp_pins.put(28, value);
+		temp_pins.put(29, value);
 	}
 	
 
@@ -23,7 +27,7 @@ public class Pin {
 
 		
 		// Load list of available pins for the specific locker
-		Integer[] available_pins = pins.get(locker_id);
+		Integer[] available_pins = temp_pins.get(locker_id);
 		// Used to track index
 		int current_index = 0;
 
@@ -44,6 +48,25 @@ public class Pin {
 			return available_pins[current_index + 1];
 		}
 	}
+
+	/**
+	 * Verifies the user's ability to access the pins
+	 * @return Boolean result of the verification
+	 */
+	private boolean verifyUser() {
+		boolean result = false;
+
+		// Allow user to input password
+		String entry = JOptionPane.showInputDialog(null, "Please enter the pin password");
+
+		// If the user entry is equivalent to the password
+		if (entry != null && entry.equals(password)) {
+			// Give user access
+			result = true;
+		}
+
+		return result;
+	}
 	
 	//private Map<Integer, Integer[]> loadPins();
 	
@@ -51,6 +74,8 @@ public class Pin {
 
 	public static void main(String[] args) {
 		Pin pin_test = new Pin();
+
+		System.out.println(pin_test.verifyUser());
 
 		// Testing that next pin is selected
 		int next_pin = pin_test.getNextPin(1111, 28);
